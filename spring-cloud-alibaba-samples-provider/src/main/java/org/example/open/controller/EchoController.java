85ac3597e0662b15072f0059b3e19dbd867cf32f
‘xingyehuang’ <974996042@qq.com>
2023-03-27 23:11:58 +0800

    springCloud alibaba方式集成sentinel

--- spring-cloud-alibaba-samples-provider/src/main/java/org/example/open/controller/EchoController.java
+++ spring-cloud-alibaba-samples-provider/src/main/java/org/example/open/controller/EchoController.java
@@ -1,11 +1,9 @@
 package org.example.open.controller;
 
 import org.example.EchoService;
+import org.example.service.impl.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("echo")
@@ -17,5 +15,13 @@ public class EchoController {
     @GetMapping("/test")
     public String echo(@RequestParam("name") String name) {
         return echoService.echo(name);
+    }
+
+    @Autowired
+    private TestService service;
+
+    @GetMapping(value = "/hello/{name}")
+    public String apiHello(@PathVariable String name) {
+        return service.sayHello(name);
     }
 }
---
+++ spring-cloud-alibaba-samples-provider/src/main/java/org/example/service/impl/TestService.java
@@ -0,0 +1,13 @@
+package org.example.service.impl;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+    @SentinelResource(value = "sayHello")
+    public String sayHello(String name) {
+        return "Hello, " + name;
+    }
+}
