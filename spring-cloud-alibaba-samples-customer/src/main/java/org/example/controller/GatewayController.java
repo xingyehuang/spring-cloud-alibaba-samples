5bba7a1ee4808774db9f3d3c356043c5d72f2bf8
‘xingyehuang’ <974996042@qq.com>
2023-04-19 23:27:32 +0800

    gateway predicates(断言)

--- spring-cloud-alibaba-samples-customer/src/main/java/org/example/controller/GatewayController.java
+++ spring-cloud-alibaba-samples-customer/src/main/java/org/example/controller/GatewayController.java
@@ -9,15 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("gateway")
 public class GatewayController {
 
-    @GetMapping("/test")
-    public String test(@RequestParam("name") String name) {
+    @GetMapping("/predicate")
+    public String predicate(@RequestParam("name") String name) {
         return "gatewaytest:" + name;
     }
 
-    @GetMapping("/test2")
-    public String test2(@RequestParam("name") String name) {
-        return "gatewaytest2:" + name;
-    }
 
 
 }
